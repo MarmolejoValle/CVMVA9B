2262b4340386b5e2a2c8ce7d0e053a72922e9068
MarmolejoValle <20223tn132@utez.edu.mx>
2025-05-27 16:00:53 -0600

    [FIRST COMMIT ] first commit with all files

---
+++ src/main/java/mx/edu/utez/cvmva9b/Cvmva9BApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvmva9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvmva9BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvmva9BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvmva9b/Cvmva9BApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvmva9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvmva9BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
